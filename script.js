2aa574bd1280ead51f624b00ce8a9a40bcc57def
Shaxhwat7 <sharmashashwat929@gmail.com>
2024-11-21 21:08:35 +0530

    Done

---
+++ script.js
@@ -0,0 +1,7 @@
+function togglemenu(){
+    const menu=document.querySelector(".menulinks");
+    const icon=document.querySelector(".hambugericon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
